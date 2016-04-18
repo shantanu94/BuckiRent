bbd6ae6ddc2757d167293473db885cdc4586aaa9
Grant Stenroos <GStenroos@gmail.com>
2016-04-18 17:19:34 -0400

    very simple search functionality added

--- BuckiRent/app/models/listing.rb
+++ BuckiRent/app/models/listing.rb
@@ -2,4 +2,13 @@ class Listing < ActiveRecord::Base
   has_attached_file :photo
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
   belongs_to :user
+
+  def self.search(query)
+    if query != nil then
+      where("address like ?", "%#{query}%")
+    else
+      having("id > 0").group('id')
+    end
+  end
 end
+
